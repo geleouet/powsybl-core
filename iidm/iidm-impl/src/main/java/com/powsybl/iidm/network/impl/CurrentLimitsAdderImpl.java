ff82f7e23b350c0f7a67b226e2fc42f605d71976
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-06-18 16:53:07 +0200

    Impl of apparent power limits added
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/CurrentLimitsAdderImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/CurrentLimitsAdderImpl.java
@@ -7,173 +7,32 @@
 package com.powsybl.iidm.network.impl;
 
 import com.powsybl.iidm.network.*;
-import com.powsybl.iidm.network.LoadingLimits.TemporaryLimit;
-import com.powsybl.iidm.network.impl.CurrentLimitsImpl.TemporaryLimitImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Comparator;
-import java.util.Optional;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 /**
  *
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
-public class CurrentLimitsAdderImpl implements CurrentLimitsAdder {
-
-    private static final Comparator<Integer> ACCEPTABLE_DURATION_COMPARATOR = (acceptableDuraction1, acceptableDuraction2) -> acceptableDuraction2 - acceptableDuraction1;
+public class CurrentLimitsAdderImpl extends AbstractLoadingLimitsAdder<CurrentLimits, CurrentLimitsAdder> implements CurrentLimitsAdder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CurrentLimitsAdderImpl.class);
-
-    private final OperationalLimitsOwner owner;
-
-    private double permanentLimit = Double.NaN;
-
-    private final TreeMap<Integer, TemporaryLimit> temporaryLimits = new TreeMap<>(ACCEPTABLE_DURATION_COMPARATOR);
-
-    public class TemporaryLimitAdderImpl implements TemporaryLimitAdder<CurrentLimitsAdder> {
-
-        private String name;
-
-        private double value = Double.NaN;
-
-        private Integer acceptableDuration;
-
-        private boolean fictitious = false;
-
-        private boolean ensureNameUnicity = false;
-
-        @Override
-        public TemporaryLimitAdder<CurrentLimitsAdder> setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        @Override
-        public TemporaryLimitAdder<CurrentLimitsAdder> setValue(double value) {
-            this.value = value;
-            return this;
-        }
-
-        @Override
-        public TemporaryLimitAdder<CurrentLimitsAdder> setAcceptableDuration(int acceptableDuration) {
-            this.acceptableDuration = acceptableDuration;
-            return this;
-        }
-
-        @Override
-        public TemporaryLimitAdder<CurrentLimitsAdder> setFictitious(boolean fictitious) {
-            this.fictitious = fictitious;
-            return this;
-        }
-
-        @Override
-        public TemporaryLimitAdder<CurrentLimitsAdder> ensureNameUnicity() {
-            this.ensureNameUnicity = true;
-            return this;
-        }
-
-        @Override
-        public CurrentLimitsAdderImpl endTemporaryLimit() {
-            if (Double.isNaN(value)) {
-                throw new ValidationException(owner, "temporary limit value is not set");
-            }
-            if (value <= 0) {
-                throw new ValidationException(owner, "temporary limit value must be > 0");
-            }
-            if (acceptableDuration == null) {
-                throw new ValidationException(owner, "acceptable duration is not set");
-            }
-            if (acceptableDuration < 0) {
-                throw new ValidationException(owner, "acceptable duration must be >= 0");
-            }
-            checkAndGetUniqueName();
-            temporaryLimits.put(acceptableDuration, new TemporaryLimitImpl(name, value, acceptableDuration, fictitious));
-            return CurrentLimitsAdderImpl.this;
-        }
-
-        private void checkAndGetUniqueName() {
-            if (name == null) {
-                throw new ValidationException(owner, "name is not set");
-            }
-            if (ensureNameUnicity) {
-                int i = 0;
-                String uniqueName = name;
-                while (i < Integer.MAX_VALUE && nameExists(uniqueName)) {
-                    uniqueName = name + "#" + i;
-                    i++;
-                }
-                name = uniqueName;
-            }
-        }
-
-        private boolean nameExists(String name) {
-            return temporaryLimits.values().stream().anyMatch(t -> t.getName().equals(name));
-        }
-    }
 
     /**
      * @deprecated Use {@link #CurrentLimitsAdderImpl(OperationalLimitsOwner)} instead.
      */
     @Deprecated
     public <S, O extends CurrentLimitsOwner<S>> CurrentLimitsAdderImpl(S side, O owner) {
-        this.owner = owner;
+        super(owner);
     }
 
     public CurrentLimitsAdderImpl(OperationalLimitsOwner owner) {
-        this.owner = owner;
+        super(owner);
     }
 
     @Override
-    public double getPermanentLimit() {
-        return permanentLimit;
-    }
-
-    @Override
-    public CurrentLimitsAdder setPermanentLimit(double limit) {
-        this.permanentLimit = limit;
-        return this;
-    }
-
-    @Override
-    public double getTemporaryLimitValue(int acceptableDuration) {
-        return Optional.ofNullable(temporaryLimits.get(acceptableDuration)).map(TemporaryLimit::getValue).orElse(Double.NaN);
-    }
-
-    @Override
-    public TemporaryLimitAdder<CurrentLimitsAdder> beginTemporaryLimit() {
-        return new TemporaryLimitAdderImpl();
-    }
-
-    private void checkTemporaryLimits() {
-        // check temporary limits are consistents with permanent
-        double previousLimit = Double.NaN;
-        for (TemporaryLimit tl : temporaryLimits.values()) { // iterate in ascending order
-            if (tl.getValue() <= permanentLimit) {
-                LOGGER.debug("{}, temporary limit should be greather than permanent limit", owner.getMessageHeader());
-            }
-            if (Double.isNaN(previousLimit)) {
-                previousLimit = tl.getValue();
-            } else {
-                if (tl.getValue() <= previousLimit) {
-                    LOGGER.debug("{} : temporary limits should be in ascending value order", owner.getMessageHeader());
-                }
-            }
-        }
-        // check name unicity
-        temporaryLimits.values().stream()
-                .collect(Collectors.groupingBy(TemporaryLimit::getName))
-                .forEach((name, temporaryLimits1) -> {
-                    if (temporaryLimits1.size() > 1) {
-                        throw new ValidationException(owner, temporaryLimits1.size() + "temporary limits have the same name " + name);
-                    }
-                });
-    }
-
-    @Override
-    public CurrentLimits add() {
+    public CurrentLimitsImpl add() {
         ValidationUtil.checkPermanentLimit(owner, permanentLimit);
         checkTemporaryLimits();
         CurrentLimitsImpl limits = new CurrentLimitsImpl(permanentLimit, temporaryLimits, owner);
