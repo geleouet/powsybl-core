7d0ce14166859ccde5571a9b78fe0cff7ec20fdc
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-06-19 10:01:10 +0200

    Small correction impl (Breaking change)
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/CurrentLimitsAdderImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/CurrentLimitsAdderImpl.java
@@ -7,25 +7,12 @@
 package com.powsybl.iidm.network.impl;
 
 import com.powsybl.iidm.network.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 /**
  *
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public class CurrentLimitsAdderImpl extends AbstractLoadingLimitsAdder<CurrentLimits, CurrentLimitsAdder> implements CurrentLimitsAdder {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CurrentLimitsAdderImpl.class);
-
-    /**
-     * @deprecated Use {@link #CurrentLimitsAdderImpl(OperationalLimitsOwner)} instead.
-     */
-    @Deprecated
-    public <S, O extends CurrentLimitsOwner<S>> CurrentLimitsAdderImpl(S side, O owner) {
-        super(owner);
-    }
 
     public CurrentLimitsAdderImpl(OperationalLimitsOwner owner) {
         super(owner);
