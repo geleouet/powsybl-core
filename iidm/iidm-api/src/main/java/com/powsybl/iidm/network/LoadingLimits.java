0ccd47aa67616ea6ec01faaa8258acae0ce66602
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-06-23 16:57:17 +0200

    Add copyright and authors
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/LoadingLimits.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/LoadingLimits.java
@@ -1,7 +1,17 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.iidm.network;
 
 import java.util.Collection;
 
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ * @author Thibaut Vermeulen <thibaut.vermeulen at rte-france.com>
+ */
 public interface LoadingLimits extends OperationalLimits {
 
     /**
